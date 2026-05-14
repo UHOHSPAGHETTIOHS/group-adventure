b2be58a2ee94ff8701b122be57c8a0ffe647a48f
Dawson Smith <dawsonmsmith@protonmail.com>
2026-05-13 21:40:51 -0400

    new

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -1,3 +1,8 @@
-import { kv } from '@vercel/kv';
+// src/lib/kv.ts
+import { Redis } from '@upstash/redis';
+
+// Using `Redis.fromEnv()` automatically reads
+// UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN from your environment
+const kv = Redis.fromEnv();
 
 export default kv;
